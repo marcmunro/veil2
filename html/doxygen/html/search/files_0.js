38361bd58cdb64c9e14ed7dd1fc8579701fe969e
Marc Munro <marc@bloodnok.com>
2020-12-29 16:33:53 -0800

    Final??? doc release attempt for v0.9.2

--- html/doxygen/html/search/files_0.js
+++ html/doxygen/html/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['query_2ec',['query.c',['../query_8c.html',1,'']]]
+  ['query_2ec',['query.c',['../query_8c.html',1,'']]],
+  ['query_2ed',['query.d',['../query_8d.html',1,'']]]
 ];
